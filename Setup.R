d0f006bdb6dce8faf7d71e66b2522070d3b0381c
rajewski <rajewski23@gmail.com>
2020-10-28 22:18:18 -0700

    original R file to make dirs and starter files

---
+++ Setup.R
@@ -0,0 +1,10 @@
+install.packages("devtools")
+devtools::install_github("nstrayer/datadrivencv")
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Alex Rajewski",
+  data_location = "https://docs.google.com/spreadsheets/d/1rn6s548jcW0THDsa-juUtDRC4CovcMGCg4QElyH2nqg/",
+  pdf_location = "https://github.com/rajewski/resume/rajewski_cv.pdf",
+  html_location = "http://rajewski.github.io/CV/",
+  source_location = "https://github.com/rajewski/resume"
+)
